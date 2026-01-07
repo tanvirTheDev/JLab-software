32dad449e4debb264f94f9ba6521af5b687ca5b9
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2026-01-07 16:52:49 +0530

    add homepage and contact pag

--- src/components/Shared/Header.tsx
+++ src/components/Shared/Header.tsx
@@ -22,6 +22,7 @@ export function Header() {
     { name: "Services", href: "#services" },
     { name: "Work", href: "#work" },
     { name: "About", href: "#about" },
+    { name: "Contact", href: "/contact" },
   ];
 
   return (
